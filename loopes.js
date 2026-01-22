144a99c284babd718a4fdfaf05f553af273f70b8
andaniurvi18-glitch <andaniurvi18@gmail.com>
2026-01-22 16:34:42 +0530

    loopes

---
+++ loopes.js
@@ -0,0 +1,102 @@
+// loops repeating code blocks
+// 1 2 3 4 5 6
+// for loop
+// why use for Loop? when you know how many times you want to repeat a block // print 1 to 10-> console.log(1); console.log(2); console.log(10);
+// kaya thi javanu chhe --> kaya sudhi javanu chhe --> kevi rite javanu chhe
+// for (ex. 1 -> 50 -> increment by 1)
+//(start; end; change)
+// for (let i=1; i <= 10; i++) {
+// console.log(i);
+// }
+// for(let i= 20; i >=10; i--){
+// console.log(i)
+// }
+
+// while loop -- do while loop
+// kaya thi javanu chhe --> Kaya rokavanu chhe --> Kevi rite javanu chhe
+// while (ex. 1 -> Hello world -> stop when condition false)
+// start
+// while(end) {
+// code
+// change
+// }
+// let j = 10;
+// while (j <= 20) {
+// console.log("j:", j);
+// j++;
+// }
+
+
+
+// loop
+// Ex:1
+// let k = 10;
+// while (k <= 20){
+//     console.log(k);
+//     k++;
+// }
+
+//ex:2
+// let k = 10;
+// while (k<= 20){
+//     console.log(k);
+//     k++;
+// }
+
+//ex:3
+// let ao = 6;
+// while (ao > 1){
+//     console.log(ao);
+//     ao--;
+// }
+
+//ex:4
+//  let d= "Hello";
+//  let i=0;
+//  while (i < 20)  {
+//  console.log(d);
+//  i++;
+//  }
+
+
+//Ex:5
+// let d = 20;
+// while (d >= 20 && d <= 25) {
+//console.log(d);
+//d++;
+// }
+
+//ex:6
+// let q = 50;
+// while (q <= 60) {
+//console.log(q);
+//q++
+// }
+
+//do while loop
+// do {} while (end);
+// start
+// do{
+// code
+// change}
+// while(end);
+let g = 10;
+do {
+console.log("do while : ", g);
+g++;
+} while (g <= 20);
+let h = 10;
+do {
+console.log("do while: ", h);
+h--;
+} while (h >= 1);
+
+
+
+
+
+
+
+
+
+
